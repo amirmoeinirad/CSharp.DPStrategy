8a66fce4aeef6c0eb927d664cc8b26ab7c84bb0c
amir <amirmoinirad@gmail.com>
2026-05-11 17:45:59 +0330

    Minor Edits

--- StrategyDP/Program.cs
+++ StrategyDP/Program.cs
@@ -1,7 +1,7 @@
 // Amir Moeini Rad
 // December, 2025
 
-// Main Concept: The Strategy Design Pattern
+// The Strategy Design Pattern
 
 // In this pattern, we define a family of algorithms, encapsulate each one in a class,
 // and make them interchangeable.
@@ -11,21 +11,23 @@ namespace DPStrategy
     // Strategy interface
     public interface IDiscountStrategy
     {
-        decimal ApplyDiscount(decimal price);
+        decimal ApplyDiscount(decimal price, decimal discountPercent);
     }
     
+
 
     // Concrete strategies
     public class NoDiscount : IDiscountStrategy
     {
-        public decimal ApplyDiscount(decimal price) => price;
+        public decimal ApplyDiscount(decimal price, decimal discountPercent = 0) => price;
     }
 
     public class PercentageDiscount : IDiscountStrategy
     {
-        public decimal ApplyDiscount(decimal price) => price * 0.9m; // 10% off
+        public decimal ApplyDiscount(decimal price, decimal discountPercent) => (price - price * discountPercent);
     }
     
+
 
     // Context class
     public class ShoppingCart
@@ -37,9 +39,10 @@ namespace DPStrategy
             _strategy = strategy;
         }
 
-        public decimal Checkout(decimal price) => _strategy.ApplyDiscount(price);
+        public decimal Checkout(decimal price, decimal discountPercent = 0) => _strategy.ApplyDiscount(price, discountPercent);
     }
     
+
 
     // Client
     internal class Program
@@ -56,12 +59,12 @@ namespace DPStrategy
             IDiscountStrategy discountStrategy = new NoDiscount();            
             var cart = new ShoppingCart(discountStrategy);
             decimal finalPrice = cart.Checkout(price);
-            Console.WriteLine($"Final Price: {finalPrice}");
+            Console.WriteLine($"Final Price w/o discount: {finalPrice}");
 
             discountStrategy = new PercentageDiscount();
             cart = new ShoppingCart(discountStrategy);
-            finalPrice = cart.Checkout(price);
-            Console.WriteLine($"Final Price: {finalPrice}");
+            finalPrice = cart.Checkout(price, 0.10m);
+            Console.WriteLine($"Final Price w discount: {finalPrice}");
 
             Console.WriteLine("\nDone.");
         }
